77279cf9c8c3bc6e79e855ed540dbdec22ade2b2
alicanaydin <alican.aydin010@gmail.com>
2026-02-11 10:29:46 +0300

    Initial Commit

---
+++ SayimUzungil2/SayimUzungil2App.swift
@@ -0,0 +1,17 @@
+//
+//  SayimUzungil2App.swift
+//  SayimUzungil2
+//
+//  Created by Ali Can Aydın on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SayimUzungil2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
